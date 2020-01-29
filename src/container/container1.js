d1555f3aa2e2eaa793916dccab5c62479540a84a
Rashid-Muhammad <Rashid.Muhammad@autotrader.com>
2020-01-29 16:27:41 -0500

    Added container 2

--- src/container/container1.js
+++ src/container/container1.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
+import Component1 from '../functional/component1'
 
 class Container1 extends Component {
-    render() {
-        return (
-            <div>{this.props.nickname}</div>
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            stateprop1: "Our Initial State",
+            stateprop2: 5
+        }
+    }
+
+    changeState = () => {
+        this.setState((prevState,props) => ({
+            stateprop2 : prevState.stateprop2 + 1,
+            stateprop1 : this.state.stateprop1 + "L"})
         )
     }
+
+    changeState2 = () => {
+        this.setState({
+            stateprop1 : this.state.stateprop1 + "L"
+        })
+        
+    }
+
+    render() {
+        return (
+           
+            <div>
+                <button onClick={() => this.changeState()}>Change State</button>
+                <button onClick={() => this.changeState2()}>Change State 2</button>
+                <br />
+                {this.state.stateprop2}
+                <br />
+                {this.state.stateprop1}
+            </div>
+        
+        )}
 }
 
 export default Container1;
